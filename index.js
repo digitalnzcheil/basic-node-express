2f46036a1f7aa79467f41f84e1b8474b7c5b9a6e
digitalnzcheil <digitalnz@cheil.com>
2024-11-18 14:31:05 +1300

    set to take PORT from process.env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 // Middleware
 app.use(cors()); // Handle CORS
